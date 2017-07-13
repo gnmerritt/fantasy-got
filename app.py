83e0cf5553bff0bc9e3e8e64e4a9e7e4d45972e6
Nathan Merritt <gnm@the-merritts.net>
2017-07-12 22:47:45 -0400

    Add character picking endpoint

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import csv
 import jsonpickle
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 STATE = 'state.pickle'
@@ -22,6 +22,12 @@ def read_state():
         return jsonpickle.decode(state.read())
 
 
+def save_state(state):
+    frozen = jsonpickle.encode(state)
+    with open(STATE, 'w') as out:
+        out.write(frozen)
+
+
 @app.route('/teams')
 def game_info():
     state = read_state()
@@ -40,10 +46,31 @@ def characters():
     return jsonify(read_chars())
 
 
+@app.route('/pick', methods=['POST'])
+def pick_char():
+    if not request.json:
+        return 'No JSON body found', 400
+    character_names = [n['name'] for n in read_chars()]
+    char = request.json['char']
+    if char not in character_names:
+        print(character_names)
+        return "Character '{}' not found".format(char), 404
+    teams = read_teams()
+    team = request.json['team']
+    if team not in teams:
+        return "Team '{}' not found".format(team), 404
+    state = read_state()
+    for t, picked in state.items():
+        if char in picked:
+            return "{} already on {}".format(char, t), 400
+
+    state[team].append(char)
+    save_state(state)
+    return "Drafting {} to {}".format(char, team), 200
+
+
 if __name__ == "__main__":
     print("Resetting game state at " + STATE)
     teams = read_teams()
     undrafted = {t: [] for t in teams}
-    frozen = jsonpickle.encode(undrafted)
-    with open(STATE, 'w') as out:
-        out.write(frozen)
+    save_state(undrafted)
